a01952535e8ee912e8a1eda9883de82958669944
sayeesh007 <67954353+sayeesh007@users.noreply.github.com>
2024-03-13 17:11:00 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -81,7 +81,7 @@ const Banner = () => {
                 Linkedin <FaLinkedin />
               </button>
                 </a>
-                <a href="https://drive.google.com/file/d/1OjSDq3lbULQUSczdRcGSpGxcUtO9bL-e/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1TJBqcYfb48tHRgpDhF7AjQECg8pXPWHF/view?usp=sharing" target="_blank" rel="noopener noreferrer">
               <button className="btn btn-sm lg:btn-lg flex justify-center items-center">
                 Resume <FaClipboard />
               </button>
